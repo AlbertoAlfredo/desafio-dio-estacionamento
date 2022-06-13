b83c1a9cfa3bad2a264fb6582ed56580b8cb2de5
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-06-13 11:14:50 -0300

    Continuação do desafio

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+(function () {
+    var _a;
+    const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() { }
+        ;
+        function adicionar(veiculo) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td>
+                    <Button class='excluir'>Excluir</Button>
+                </td>
+            `;
+        }
+        ;
+        function remover() { }
+        ;
+        function salvar() { }
+        ;
+        function render() { }
+        ;
+        return { ler, adicionar, remover, salvar, render };
+    }
+    (_a = $('#cadastrar')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        var _a, _b;
+        const nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
+        const placa = (_b = $('#placa')) === null || _b === void 0 ? void 0 : _b.value;
+        if (!nome || !placa) {
+            alert('Os campos nome e placa são obrigatórios.');
+            return;
+        }
+        patio().adicionar({ nome, placa, entrada: new Date() });
+    });
+})();
---
+++ script.ts
@@ -0,0 +1,41 @@
+interface Veiculo{
+    nome: string;
+    placa: string;
+    entrada: Date;
+}
+(function () {
+    const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function patio(){
+        function ler(){};
+        function adicionar(veiculo: Veiculo){
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td>
+                    <Button class='excluir'>Excluir</Button>
+                </td>
+            `
+        };
+        function remover(){};
+        function salvar(){};
+        function render(){};
+
+        return{ler, adicionar, remover, salvar, render}
+    }
+
+    $('#cadastrar')?.addEventListener('click', () => {
+        const nome = $('#nome')?.value;
+        const placa = $('#placa')?.value;
+
+        if(!nome || !placa){
+            alert('Os campos nome e placa são obrigatórios.')
+            return;
+        }
+        patio().adicionar({ nome, placa, entrada: new Date() })
+    }) 
+
+})()
